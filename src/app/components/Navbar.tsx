5aa5b34b9060d9cc86eb5532a545eded000edd1d
Md Repon <repon7253@gmail.com>
2024-10-15 21:57:03 +0600

    add dark mode

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,15 +18,28 @@ const Navbar = ({ className }: { className?: string }) => {
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-          <MenuItem setActive={setActive} active={active} item="Home">
-            
-          </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
         </Link>
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/course">All Courses</HoveredLink>
+            <HoveredLink href="/course">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/course">Advanced Composition</HoveredLink>
+            <HoveredLink href="/course">Songwriting</HoveredLink>
+            <HoveredLink href="/course">Music Production</HoveredLink>
           </div>
         </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
       </Menu>
     </div>
   );
