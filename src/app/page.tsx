1b1d4df219ca9609eb7a03d093f5edb30b8775da
Md Repon <repon7253@gmail.com>
2024-10-20 20:56:53 +0600

    Grid background complite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeatureCourses from "./components/FeatureCourses";
 import HeroSection from "./components/HeroSection";
+import TestimonialCard from "./components/TestimonialCard";
 import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
@@ -7,7 +8,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeatureCourses />
-      <WhyChooseUs/>
+      <WhyChooseUs />
+      <TestimonialCard/>
     </main>
   );
 }
